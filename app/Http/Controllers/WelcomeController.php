4e7f127b067a6f3db2654d9b1d1d5a69a87275a2
FalkSturmfels <fritzachim2@gmail.com>
2016-03-25 22:03:06 +0100

    Tutorial Part 3: Implemented first controller and changed index route to it.

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Achim
+ * Date: 25.03.2016
+ * Time: 21:59
+ */
+
+namespace App\Http\Controllers;
+
+
+class WelcomeController extends Controller
+{
+
+    public function index()
+    {
+        return view('Welcome');
+    }
+}
