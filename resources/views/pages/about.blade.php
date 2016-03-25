dc342c077e3b513392347507a59de71bd0a7c07f
FalkSturmfels <fritzachim2@gmail.com>
2016-03-25 22:45:09 +0100

    Tutorial Part 4: Added new 'PageController' controller with action 'about' and new view 'about.blade.php'

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>About me</h1>
+
+<p>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</br>
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore</br>
+    magna aliquyam erat, sed diam voluptua. At vero eos et accusam</br>
+    et justo duo dolores et ea rebum. Stet clita kasd gubergren,</br>
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.</br>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</br>
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore</br>
+    magna aliquyam erat, sed diam voluptua.</br>
+    At vero eos et accusam et justo duo dolores et ea rebum.</br>
+    Stet clita kasd gubergren, no sea takimata sanctus est Lorem </br>
+    ipsum dolor sit amet.
+</p>
+
+</body>
+</html>
