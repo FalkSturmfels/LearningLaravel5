6b0edc5d55325786b0cda00a3668fae88784827a
FalkSturmfels <fritzachim2@gmail.com>
2016-03-26 22:33:31 +0100

    Tutorial Part 5: Used blade control statements to show a list of people,if the list isn't empty.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,10 +10,16 @@ class PagesController extends Controller
 {
     public function about()
     {
-        $first = 'Fox';
-        $last = 'Mulder';
+        $people = [
+            'John Doe',
+            'Frank Bogart',
+            'Lucy MacQuire',
+            'Julia Franklin',
+            'Max Buche',
+            'Lisa Maywather'
+        ];
 
-        return view('pages/about', compact('first', 'last'));
+        return view('pages/about', compact('people'));
     }
 
     public function contact()
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,29 @@
 @extends('app')
 
 @section('content')
-<h1>About me: {{ $first }} {{ $last }}</h1>
+<h1>About </h1>
+
+@if(count($people) > 0)
+<h3>People I like:</h3>
+
+<ul>
+    @foreach($people as $person)
+        <li>{{$person}}</li>
+    @endforeach
+</ul>
+@endif
 
 <p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</br>
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore</br>
-    magna aliquyam erat, sed diam voluptua. At vero eos et accusam</br>
-    et justo duo dolores et ea rebum. Stet clita kasd gubergren,</br>
-    no sea takimata sanctus est Lorem ipsum dolor sit amet.</br>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</br>
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore</br>
-    magna aliquyam erat, sed diam voluptua.</br>
-    At vero eos et accusam et justo duo dolores et ea rebum.</br>
-    Stet clita kasd gubergren, no sea takimata sanctus est Lorem </br>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore<br>
+    magna aliquyam erat, sed diam voluptua. At vero eos et accusam<br>
+    et justo duo dolores et ea rebum. Stet clita kasd gubergren,<br>
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore<br>
+    magna aliquyam erat, sed diam voluptua.<br>
+    At vero eos et accusam et justo duo dolores et ea rebum.<br>
+    Stet clita kasd gubergren, no sea takimata sanctus est Lorem <br>
     ipsum dolor sit amet.
 </p>
 @stop
