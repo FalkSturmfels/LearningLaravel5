79aa58ca0071bc04ab13fb702f13b01cc45c477e
achim.fritz <fritzachim2@gmail.com>
2016-06-09 13:16:52 +0200

    Tutorial Part 10: Enhanced articles create form with text field for title and text area for body

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,10 +4,18 @@
 
     <h1>Create new article</h1>
 
-    </hr>
+    <hr/>
 
     {!! Form::open() !!}
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
 
+    <div class="form-group">
+        {!! Form::label('body', 'Body:') !!}
+        {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+    </div>
     {!! Form::close() !!}
 
 @stop
