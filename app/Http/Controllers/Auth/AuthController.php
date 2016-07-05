d90c3a8b97800637314bc6c150a67c765f30c410
achim.fritz <fritzachim2@gmail.com>
2016-07-05 13:00:25 +0200

    Tutorial Part 15: Changed name to username in AuthController and register form view.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,6 +21,8 @@ class AuthController extends Controller
     |
     */
 
+    protected $redirectPath = '/articles';
+
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     /**
@@ -49,7 +51,7 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'username' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -64,7 +66,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
