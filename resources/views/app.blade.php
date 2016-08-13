6c96356994af8b6da0b3c5af0d5a7e477103ccbb
FalkSturmfels <fritzachim2@gmail.com>
2016-08-13 21:28:04 +0200

    Added bootstrap with theme "slate" from bootswatch

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <!-- Referencing Bootstrap CSS that is hosted locally -->
+    {{ Html::style('css/bootstrap.min.css') }}
 </head>
 <body>
-
 <div class="container">
     @yield('content')
 </div>
 
-    @yield('footer')
+@yield('footer')
 
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"
+        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+        crossorigin="anonymous"></script>
+
+<!-- Referencing Bootstrap JS that is hosted locally -->
+{{ Html::script('js/bootstrap.min.js') }}
 </body>
 </html>
