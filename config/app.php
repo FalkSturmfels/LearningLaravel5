9b03eef9310c7834ccc7a474fbc1aff6aae12df8
FalkSturmfels <fritzachim2@gmail.com>
2016-08-27 22:58:15 +0200

    Part 25 Created a dedicated view composer service provider

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ViewComposerServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
 
